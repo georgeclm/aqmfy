a2537372715e540234c00b7f27f30d5d830d2d9a
George <cavidjaja@gmail.com>
2021-03-22 20:28:09 +0700

    t

--- resources/views/service/index.blade.php
+++ resources/views/service/index.blade.php
@@ -42,7 +42,43 @@
     <div class="container mb-5 mt-5">
         <div class="col-md-12">
             <h3 class='mb-4'>Trending Services</h3>
-            <x-services :services="$services" />
+            <div class="row row-cols-1 row-cols-md-6">
+                @if ($services->count())
+                    <div class="col-md-12 text-muted mb-3">Found {{ $services->count() }} results</div>
+
+                    @foreach ($services as $service)
+
+                        <div class="col mb-4 link-web">
+                            <a href="{{ route('services.show', $service) }}">
+                                <div class="card h-100 rounded" style="width: 12rem;">
+                                    <img src="{{ asset("storage/product/{$service->image}") }}" class="card-img-top"
+                                        style="width: 12rem; height: 12rem; background-size: cover; background-position: center;">
+                                    <div class="card-body">
+                                        <h6 class="card-title">{{ $service->name }}</h6>
+                                        <h5 class="card-text"> Rp. {{ number_format($service->price) }}</h5>
+                                        @php
+                                            $average = $service->ratings->average('rating');
+                                        @endphp
+                                    </div>
+
+                                    @if ($average != 0)
+                                        <div class="card-footer">
+                                            <div class="ratingindex">
+                                                <span class="icon">★</span>
+                                            </div>
+                                            <span class=""><strong> {{ $average }}</strong><span
+                                                    class="text-muted">({{ $service->ratings->count() }})</span></span>
+                                        </div>
+                                    @endif
+                                </div>
+                            </a>
+                        </div>
+                    @endforeach
+                @else
+                    <div class="h4">Service Not Found</div>
+                @endif
+            </div>
+
 
         </div>
     </div>

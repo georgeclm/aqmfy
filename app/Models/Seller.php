f196484b1f0c0d318ffddc5515f61987110e5f10
George <cavidjaja@gmail.com>
2021-03-22 21:03:32 +0700

    t

--- app/Models/Seller.php
+++ app/Models/Seller.php
@@ -12,7 +12,7 @@ class Seller extends Model
     {
         // folder inside the public path for default image
         $imagePath = ($this->image) ? $this->image : 'jAZHCrXvUSsoh3BtdypreKvz8tz0M4DEnDOfvvDt.png';
-        return 'storage/' . $imagePath;
+        return 'storage/app/public' . $imagePath;
     }
 
     protected $guarded = [];
